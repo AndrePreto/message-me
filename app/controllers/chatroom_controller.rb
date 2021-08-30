12e04efa3fb484697def50aa2d4550f9e34e1a82
AndrePreto <a.preto@campus.fct.unl.pt>
2021-08-30 17:42:29 +0100

    feature/logout-action-restrictions - Actions restricted to logged in users. Users can now use messages

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,9 @@
 class ChatroomController < ApplicationController
-  
+  before_action :require_user
+
+  # GET / -> root_path
   def index
+    @message = Message.new
     @messages = Message.all.order('id asc')
   end
   
