8ba29d75a0538326d2206e1e7ff90fcaa7564309
AndrePreto <a.preto@campus.fct.unl.pt>
2022-04-19 15:42:29 +0100

    feature/action-cable-chatroom - final commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,17 +18,30 @@
 //= require_tree .
 
 function scroll_bottom() {
-  if ($('#messages').length > 0) {
-    $('#messages').scrollTop($('#messages')[0].scrollHeight)
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
   }
 }
 
-document.addEventListener("turbolinks:load", function() {
-  $('.ui.dropdown').dropdown();
-  $('.message .close').on('click', function() {
-    $(this).closest('.message').transition('fade');
+function submit_message() {
+  $("#messages_body").on("Keydown", function (e) {
+    if (e.keyCode == 13) {
+      $("#submit_message_button").click();
+    }
   });
-  scroll_bottom()
+}
+
+function pressEnter(e) {
+  if (e.keyCode == 13) {
+    $("#submit_message_button").click();
+  }
+}
+
+document.addEventListener("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown();
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
+  submit_message();
+  scroll_bottom();
 });
-
-
